30c2f67bd7db2e7d2a7fb96c61d52a55093b660a
NavyaGunda23 <52776081+NavyaGunda23@users.noreply.github.com>
2022-05-22 10:32:01 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
+import UserPage from './Components/UserPage/UserPage'
+import Checkout from './Components/checkout/Checkout';
+import ThankyouScreen from './Components/ThankyouScreen/ThankyouScreen';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <Routes>
+      <Route path="/" element={<Register />} />
+      <Route path="/UserPage/UserPage" element={<UserPage />} />
+      {/* <Route path="/cart/CartPage" element={<CartPage />} /> */}
+      <Route path="/checkout/Checkout" element={<Checkout />}/>
+      <Route path="/ThankyouScreen/ThankyouScreen" element={<ThankyouScreen />}/>
+    </Routes>
+  
+        {/* <Register /> */}
     </div>
   );
 }
